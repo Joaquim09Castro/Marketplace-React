c193512a96aefaf2257d7f178b461a4ea8ea8b6a
Joaquim Castro <joaquimcastro2909@gmail.com>
2020-11-05 00:30:37 -0200

    Login prototype

--- src/Components/ModalSignIn/ModalSignIn.js
+++ src/Components/ModalSignIn/ModalSignIn.js
@@ -1,14 +1,37 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 
 function ModalSignIn(props) {
-  const [show, setShow] = useState(false);
+  const [show, setShow] = React.useState(false);
+
+  const [ email , setEmail ] = React.useState('');
+  const [ pwrd , setPwrd ] = React.useState('');
+  const [ user , setUser ] = React.useState('');
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+
+  const submit = (event) => {
+
+    event.preventDefault();
+
+    const body = {
+      email,
+      pwrd
+    }
+    console.log(body)
+
+    fetch('https://resilia-marketplace.herokuapp.com/session/login' , {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: { "Content-Type": "application/json"}
+    })
+      .then( resp => setUser(resp.token))
+  }
 
   return (
     <>
@@ -27,10 +50,16 @@ function ModalSignIn(props) {
           <Modal.Title>Login</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <form action="https://resilia-marketplace.herokuapp.com/products" method="GET">
+          <form onSubmit={submit} id="modal-form">
             <Form.Group controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+
+              <Form.Control 
+                type="email" 
+                placeholder="Enter email" 
+                onChange={ event => setEmail(event.target.value)}
+              />
+
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
@@ -38,14 +67,23 @@ function ModalSignIn(props) {
 
             <Form.Group controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              
+              <Form.Control
+                type="password" 
+                placeholder="Password"
+                onChange={ event => setPwrd(event.target.value)}
+              />
+
             </Form.Group>
-            <Form.Group controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Enter
-            </Button>
+            
+            <input
+              type="submit"
+              className="bg-primary text-light"
+              value="Log in"
+              onClick={
+                console.log("teste")
+              }
+            />
           </form>
         </Modal.Body>        
       </Modal>
